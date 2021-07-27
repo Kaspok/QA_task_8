6dd7d90b678a56c18d816ef645b948ea23db0ee7
Kaspok <kaspok251198uU@yandex.ru>
2021-07-27 21:55:34 +0300

    Добавил зависимости для тестирования файла, добавил файл и тест на проверку содержимого

---
+++ src/test/java/tests/WordTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WordTest {
+
+    @Test
+    void junitWordTest() throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream resourceAsStream = classLoader.getResourceAsStream("TestWord.docx");
+        XWPFDocument docFile = new XWPFDocument(resourceAsStream);
+        assertThat(docFile.getDocument().toString()).contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac faucibus odio.");
+    }
+}
